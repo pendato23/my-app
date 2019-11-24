2ef03c8dfe27a43e19e17e82dac91c81740eba61
pendato23 <55184307+pendato23@users.noreply.github.com>
2019-11-25 00:27:10 +0900

    first try

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+//JSX : javascript XML
+//역활 : javascript 가 html을 제어해요.
+// 1. js에서 html을 생성하고
+// 2. js가 html을 제어해요.
 
+// 한계점 => jsx는 하나의 component만 리턴할 수 있다.
+// 2개의 컴포넌트를 리턴하는 방법. => fragment
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>hello world!</h2>
+      <p>react</p>
+      <button>블루</button>
+    </>
   );
 }
 
+// 백앤드 영역
+// 데이터베이스 연동
+// 인터넷 네트워크 연결
+//
 export default App;
